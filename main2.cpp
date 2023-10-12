5b4905f06e210f93a412d84f8fd4566b9ba306b0
ChiorgloNichita <niki.kioroglo.2004@gmail.com>
2023-10-12 11:49:28 +0300

    lab 2 first half

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+static int hello(int x) {
+    return x * 2;
+}
+
+int main() {
+    int result = hello(5);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
