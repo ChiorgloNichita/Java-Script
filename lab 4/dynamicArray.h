3f931d0b525c06c1cdfbde970d077a5009c820a0
ChiorgloNichita <niki.kioroglo.2004@gmail.com>
2023-12-10 03:04:34 +0200

    ispravil

--- lab 4/dynamicArray.h
+++ lab 4/dynamicArray.h
@@ -4,13 +4,32 @@
 #include <iostream>
 
 struct DynamicArray {
-    int* values = nullptr;
-    int length = 0;
-    int capacity = 0;
+    int* values;
+    int length;
+    int capacity;
 
-    DynamicArray(int);
+    // Функция для выделения памяти и инициализации массива
+    void init(int initialCapacity) {
+        length = 0;
+        // Убеждаемся, что capacity как минимум 1
+        capacity = (initialCapacity > 0) ? initialCapacity : 1;
+        values = new int[capacity];
+    }
 
-    void coutValues();
+    // Функция для вывода значений массива
+    void coutValues() const {
+        for (int i = 0; i < length; i++) {
+            std::cout << values[i] << " ";
+        }
+        std::cout << std::endl;
+    }
 
-    ~DynamicArray(); // Добавляем деструктор для освобождения памяти
+    // Функция для освобождения памяти
+    void freeMemory() {
+        delete[] values;
+        length = 0;
+        capacity = 0;
+        values = nullptr; 
+    }
 };
+
--- lab 4/main.cpp
+++ lab 4/main.cpp
@@ -1,60 +1,101 @@
-// main.cpp
-
 #include <iostream>
 #include <cassert>
-#include "dynamicArray.h"
+#include <span> // Добавлен заголовок для std::span
+#include <algorithm> // Добавлен заголовок для std::max
 
-DynamicArray createDynamicArray(int capacity = 5) {
-    DynamicArray* newArray = new DynamicArray(capacity);
-    return *newArray;
+struct DynamicArray {
+    int* values;
+    size_t length;
+    size_t capacity;
+
+    // Конструктор
+    DynamicArray(size_t initialCapacity = 5)
+        : values(new int[initialCapacity]), length(0), capacity(std::max<size_t>(initialCapacity, 1)) {}
+
+    // Деструктор
+    ~DynamicArray() {
+        delete[] values;
+    }
+};
+
+// Функция для создания динамического массива с указанной емкостью
+DynamicArray createDynamicArrayWithCapacity(size_t capacity = 5) {
+    return DynamicArray(std::max<size_t>(capacity, 1));
 }
 
+// Функция для добавления элемента в массив
 void addElementToArray(DynamicArray& arr, int value) {
     if (arr.length + 1 >= arr.capacity) {
+        
         arr.capacity *= 2;
         int* newValues = new int[arr.capacity];
-        for (int i = 0; i < arr.length; i++) {
+        for (size_t i = 0; i < arr.length; i++) {
             newValues[i] = arr.values[i];
         }
-        delete[] arr.values; // Освобождаем старую память
+        delete[] arr.values;
         arr.values = newValues;
     }
     arr.values[arr.length] = value;
     arr.length += 1;
 }
 
-int getElementByIndex(const DynamicArray& arr, int index) {
-    if (arr.length - 1 >= index) {
+// Функция для получения элемента по индексу
+int getElementAtIndex(const DynamicArray& arr, size_t index) {
+    if (index < arr.length) {
         return arr.values[index];
     } else {
-        return -100000;
+        return -100000; // Возвращаем значение, указывающее на ошибку 
     }
 }
 
-void destroyMemory(DynamicArray& arr) {
+// Функция для уничтожения динамического массива
+void destroyDynamicArray(DynamicArray& arr) {
     arr.capacity = 1;
     arr.length = 0;
     delete[] arr.values;
-    arr.values = nullptr; // Устанавливаем указатель в нуль после освобождения памяти
 }
-
+std::span<int> getCurrentSpan(const DynamicArray& arr) {
+    return std::span<int>(arr.values, arr.length);
+}
 int main() {
-    int cap;
-    std::cout << "Give default value for capacity (0 if default)" << std::endl;
-    std::cin >> cap;
+    // Test 1
+    DynamicArray arr1 = createDynamicArrayWithCapacity(10);
+    assert(arr1.capacity == 10);
+    std::cout << "Test 1: Create dynamic array with custom capacity - Passed" << std::endl;
 
-    DynamicArray myArray = cap == 0 ? createDynamicArray() : createDynamicArray(cap);
-    cap == 0 ? assert(myArray.capacity == 5) : assert(myArray.capacity == cap);
-    addElementToArray(myArray, 10);
-    myArray.coutValues();
-    addElementToArray(myArray, 20);
-    addElementToArray(myArray, 30);
-    myArray.coutValues();
+    // Test 2
+    DynamicArray arr2 = createDynamicArray();
+    assert(arr2.capacity == 5);
+    std::cout << "Test 2: Create dynamic array with default capacity - Passed" << std::endl;
 
-    int el = getElementByIndex(myArray, 2);
-    assert(el == 30);
+    // Test 3
+    addElementToArray(arr2, 5);
+    assert(arr2.capacity == 5);
+    addElementToArray(arr2, 10);
+    assert(arr2.capacity == 10);
+    addElementToArray(arr2, 20);
+    assert(arr2.capacity == 20);
+    std::cout << "Test 3" << std::endl;
 
-    destroyMemory(myArray);
+    // Test 4
+    int element = getElementAtIndex(arr2, 2);
+    assert(element == 20);
+    std::cout << "Test 4" << std::endl;
+
+    // Test 5
+    addElementToArray(arr2, 30);
+    std::span<int> span = getCurrentSpan(&arr2);
+    assert(span.size() == 4);
+    assert(span[0] == 5 && span[1] == 10 && span[2] == 20 && span[3] == 30);
+    std::cout << "Test 5" << std::endl;
+
+    // Test 6
+    destroyDynamicArray(&arr1);
+    destroyDynamicArray(&arr2);
+    assert(arr1.values == nullptr && arr2.values == nullptr);
+    std::cout << "Test 6" << std::endl;
+
+    std::cout << "All tests" << std::endl;
 
     return 0;
 }
